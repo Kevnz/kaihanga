2f422024137f1d4043c71e3f17bdd670d814593a
Kevin Isom <kevin.isom@gmail.com>
2019-01-14 15:34:16 +1300

    Updates

---
+++ precache-manifest.4bc6f13a58e62906482247cef8f6d3db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d07238811c02dff80e27",
+    "url": "/kaihanga/static/js/runtime~main.d0723881.js"
+  },
+  {
+    "revision": "c2856c59e3f40d4953a8",
+    "url": "/kaihanga/static/js/main.c2856c59.chunk.js"
+  },
+  {
+    "revision": "bc959a429355e9e5ab19",
+    "url": "/kaihanga/static/js/1.bc959a42.chunk.js"
+  },
+  {
+    "revision": "c2856c59e3f40d4953a8",
+    "url": "/kaihanga/static/css/main.ddbaeb16.chunk.css"
+  },
+  {
+    "revision": "69a7b2066a7bdf27b081c9e9b504e77a",
+    "url": "/kaihanga/index.html"
+  }
+];
